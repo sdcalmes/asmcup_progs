4eb1180696f99caf448c1620391e12393f876a16
Sam Calmes <sdcalmes@gmail.com>
2016-10-28 14:58:39 -0500

    finds gold and battery. Working on what comes after that. Commented out anything that breaks current functionality

--- gold_battery.asm
+++ gold_battery.asm
@@ -1,11 +1,14 @@
-; This bot can kind of escape rooms
-
+; If it spins 360 AND sees a distance of 256, we want it to move forward
+; that distance of 256
 ; Overclock the CPU to 20 cycles per second (helps with beam sensor)
-push8 #20
+push8 #100
 push8 #IO_OVERCLOCK
 io
 
-start:
+compass:
+push8 #IO_COMPASS
+io
+popf facing
 
 sensor:
 ; Read beam sensor
@@ -17,9 +20,12 @@ jmp check_type:
 
 check_type:
 push8 type
-push8 #4
+push8 #12
 and
 jnz get_gold
+pushf 0.0
+push8 #IO_MOTOR
+io
 jmp right
 
 get_gold:
@@ -27,18 +33,46 @@ get_gold:
 pushf 1.0
 push8 #IO_MOTOR
 io
+jmp sensor
+
+check_256:
+pushf -1.0
+push8 #IO_STEER
+io
+pushf 0.0
+push8 #IO_STEER
+io
+push8 #IO_SENSOR
+io
+pop8 type
+pushf 256.0
+and
+jnz wander
+
+wander:
 
 right:
- Steer right
 pushf 1.0
 push8 #IO_STEER
 io
 pushf 0.0
 push8 #IO_STEER
 io
-
+; push8 &curr_compass
+; jsr
+; pushf facing
+; pushf curr
+; and
+; jnz check_256     ;they are the same
 jmp sensor
+
+curr_compass:
+push8 #IO_COMPASS
+io
+popf curr
+ret
 
 type: dbf 0
 distance: dbf 0.0
-wander: dbf 0.0
+facing: dbf 0.0
+curr: dbf 0.0
